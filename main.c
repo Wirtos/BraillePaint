1ac7826004bd7813bc8cdbc3fbc9a2e71c3bbd82
Wirtos_new <Wirtos.new@gmail.com>
2021-04-09 18:13:22 +0300

    code cleanup

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ typedef struct braille_ctx {
     SDL_Point pnt;
 } braille_ctx;
 
-static const char *braille_map[256] = {
+static const char *const braille_map[256] = {
     "⠀",
     "⠁", "⠂", "⠃", "⠄", "⠅", "⠆", "⠇", "⠈", "⠉", "⠊", "⠋", "⠌", "⠍", "⠎", "⠏", "⠐", "⠑",
     "⠒", "⠓", "⠔", "⠕", "⠖", "⠗", "⠘", "⠙", "⠚", "⠛", "⠜", "⠝", "⠞", "⠟", "⠠", "⠡", "⠢",
@@ -30,33 +30,53 @@ static const char *braille_map[256] = {
 
 #define sz(arr) (sizeof(arr) / sizeof(*(arr)))
 
-#define BRAILLE_HEIGHT_PX 4
-#define BRAILLE_WIDTH_PX  2
-#define BSUR_WIDTH  64
-#define BSUR_HEIGHT 128
+#define WIN_W 512
+#define WIN_H 512
 
-#define WIN_WIDTH 512
-#define WIN_HEIGHT 512
+#define BRAILLE_W_PX 2
+#define BRAILLE_H_PX 4
 
-static char text[(BSUR_WIDTH * BSUR_HEIGHT) / (BRAILLE_HEIGHT_PX * BRAILLE_WIDTH_PX) * 3 + BSUR_HEIGHT + 1] = {0};
+#define BSUR_W  64
+#define BSUR_H  128
+
+#if BSUR_W % BRAILLE_W_PX != 0
+    #error "Surface width must be a multiple of 2"
+#endif
+
+#if BSUR_H % BRAILLE_H_PX != 0
+    #error "Surface height must be a multiple of 2"
+#endif
+
+static char generated_text[(BSUR_W * BSUR_H) / (BRAILLE_H_PX * BRAILLE_W_PX) * 3 + BSUR_H + 1] = {0};
 #define IS_LMB(x) ((x) & SDL_BUTTON_LMASK)
 
-static Uint32 pixeldata[BSUR_HEIGHT][BSUR_WIDTH];
+static Uint32 pixeldata[BSUR_H][BSUR_W];
+
+void clear_texture(const braille_ctx *ctx) {
+    SDL_SetRenderTarget(ctx->ren, ctx->tex);
+    SDL_SetRenderDrawColor(ctx->ren, 0x0, 0x0, 0x0, 0xFF);
+    SDL_RenderClear(ctx->ren);
+}
 
 int main(int argc, char *argv[]) {
     braille_ctx ctx = {0};
     (void) argc, (void) argv;
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-    ctx.win = SDL_CreateWindow("BraillePaint", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_SHOWN);
+    ctx.win = SDL_CreateWindow("BraillePaint",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        WIN_W, WIN_H,
+        SDL_WINDOW_SHOWN);
     ctx.ren = SDL_CreateRenderer(ctx.win, -1, SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_ACCELERATED);
-    ctx.tex = SDL_CreateTexture(ctx.ren, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, BSUR_WIDTH, BSUR_HEIGHT);
-    goto clear_tex;
+    ctx.tex = SDL_CreateTexture(ctx.ren, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, BSUR_W, BSUR_H);
+    clear_texture(&ctx);
+
     while (1) {
         SDL_Event evt;
         SDL_WaitEvent(&evt);
         switch (evt.type) {
             case SDL_QUIT:
                 goto quit;
+
             case SDL_KEYDOWN:
                 switch (evt.key.keysym.sym) {
                     case SDLK_UP:
@@ -76,39 +96,33 @@ int main(int argc, char *argv[]) {
                     case SDLK_l: {
                         SDL_Surface *sur = SDL_LoadBMP("file.bmp");
                         if (!sur) break;
-                        SDL_Texture *tex = SDL_CreateTextureFromSurface(ctx.ren, sur);
 
+                        SDL_Texture *tex = SDL_CreateTextureFromSurface(ctx.ren, sur);
                         SDL_SetRenderTarget(ctx.ren, ctx.tex);
                         SDL_RenderCopy(ctx.ren, tex, NULL, NULL);
-                        SDL_SetRenderTarget(ctx.ren, NULL);
 
                         SDL_FreeSurface(sur);
                         SDL_DestroyTexture(tex);
                         break;
                     }
                     case SDLK_c:
-                    clear_tex:
-                        SDL_SetRenderTarget(ctx.ren, ctx.tex);
-
-                        SDL_SetRenderDrawColor(ctx.ren, 0x0, 0x0, 0x0, 0xFF);
-                        SDL_RenderClear(ctx.ren);
-                        SDL_RenderPresent(ctx.ren);
-
-                        SDL_SetRenderTarget(ctx.ren, NULL);
+                        clear_texture(&ctx);
                         break;
+
                     case SDLK_s: {
                         SDL_SetRenderTarget(ctx.ren, ctx.tex);
 
-                        SDL_RenderReadPixels(ctx.ren, NULL, SDL_PIXELFORMAT_RGBA8888, pixeldata, BSUR_WIDTH * 4);
-                        char *it = text;
-                        for (int y = 0; y < BSUR_HEIGHT; y += BRAILLE_HEIGHT_PX) {
-                            for (int x = 0; x < BSUR_WIDTH; x += BRAILLE_WIDTH_PX) {
+                        SDL_RenderReadPixels(ctx.ren, NULL, SDL_PIXELFORMAT_RGBA8888,
+                            pixeldata, BSUR_W * sizeof(Uint32));
+                        char *it = generated_text;
+                        for (int y = 0; y < BSUR_H; y += BRAILLE_H_PX) {
+                            for (int x = 0; x < BSUR_W; x += BRAILLE_W_PX) {
                                 Uint8 braille_byte = 0;
-                                for (int i = x; i < x + BRAILLE_WIDTH_PX; ++i) {
-                                    for (int j = y; j < y + BRAILLE_HEIGHT_PX; ++j) {
+                                for (int i = x; i < x + BRAILLE_W_PX; ++i) {
+                                    for (int j = y; j < y + BRAILLE_H_PX; ++j) {
                                         int offset = (
-                                            BRAILLE_HEIGHT_PX * (i % BRAILLE_WIDTH_PX)
-                                            + (j % BRAILLE_HEIGHT_PX)
+                                            BRAILLE_H_PX * (i % BRAILLE_W_PX)
+                                            + (j % BRAILLE_H_PX)
                                         );
                                         braille_byte |= (pixeldata[j][i] != 0x000000FF) << offset;
                                     }
@@ -119,13 +133,12 @@ int main(int argc, char *argv[]) {
                             *it++ = '\n';
                         }
                         *it = '\0';
-                        SDL_SetClipboardText(text);
-
-                        SDL_SetRenderTarget(ctx.ren, NULL);
+                        SDL_SetClipboardText(generated_text);
                         break;
                     }
                 }
                 break;
+
             case SDL_MOUSEBUTTONDOWN:
                 if (!IS_LMB(evt.motion.state)) break;
                 ctx.pnt = (SDL_Point) {evt.button.x, evt.button.y};
@@ -140,15 +153,17 @@ int main(int argc, char *argv[]) {
 
                 SDL_GetMouseState(&ctx.pnt.x, &ctx.pnt.y);
                 filledCircleRGBA(ctx.ren,
-                    (Sint16) (ctx.pnt.x / ((double) WIN_WIDTH / BSUR_WIDTH)),
-                    (Sint16) (ctx.pnt.y / ((double) WIN_HEIGHT / BSUR_HEIGHT)),
+                    (Sint16) (ctx.pnt.x / ((double) WIN_W / BSUR_W)),
+                    (Sint16) (ctx.pnt.y / ((double) WIN_H / BSUR_H)),
                     1,
                     0x0, 0xFF, 0x0, 0xFF);
                 SDL_RenderPresent(ctx.ren);
-
-                SDL_SetRenderTarget(ctx.ren, NULL);
                 break;
-        }
+        } /* switch evt.type */
+
+        SDL_SetRenderTarget(ctx.ren, NULL);
+
+        SDL_RenderClear(ctx.ren);
         SDL_RenderCopy(ctx.ren, ctx.tex, NULL, NULL);
         SDL_RenderPresent(ctx.ren);
     }
